2efe6d8601b22cb7e12764ec9d66cc6933136f89
tonypls <toe_knee_van@hotmail.com>
2016-09-22 21:48:18 +1200

    App Setup With Map

---
+++ app/pages/home/home.ts
@@ -0,0 +1,43 @@
+import {Component, ViewChild, ElementRef} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {Geolocation} from 'ionic-native';
+
+declare var google;
+
+@Component({
+  templateUrl: 'build/pages/home/home.html'
+})
+export class HomePage {
+
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  ionViewLoaded(){
+    this.loadMap();
+  }
+
+  loadMap(){
+
+    Geolocation.getCurrentPosition().then((position) => {
+
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    }, (err) => {
+      console.log(err);
+    });
+
+  }
+
+}
