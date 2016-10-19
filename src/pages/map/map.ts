a591f7ba6799b2c5ff9baa3d1487b702067dd0d1
xvh7419 <xvh7419@autuni.ac.nz>
2016-10-19 20:14:09 +1300

    Merged code

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -72,5 +72,44 @@ export class MapPage {
       infoWindow.open(this.map, marker);
     });
   }
+  /*
+   This is the javascript code that needs to be integrated for the chat feature - Tony xD
+   import {Page} from 'ionic/ionic';
+   import {Http} from "angular2/http";
+   import {NgZone} from "angular2/core";
+
+   @Page({
+       templateUrl: 'build/pages/home/home.html',
+   })
+
+   export class HomePage {
+       constructor(http: Http) {
+           this.messages = [];
+           this.socketHost = "http://192.168.57.1:3000";
+           this.zone = new NgZone({enableLongStackTrace: false});
+           http.get(this.socketHost + "/fetch").subscribe((success) => {
+               var data = success.json();
+               for(var i = 0; i < data.length; i++) {
+                   this.messages.push(data[i].message);
+               }
+           }, (error) => {
+               console.log(JSON.stringify(error));
+           });
+           this.chatBox = "";
+           this.socket = io(this.socketHost);
+           this.socket.on("chat_message", (msg) => {
+               this.zone.run(() => {
+                   this.messages.push(msg);
+               });
+           });
+       }
+
+       send(message) {
+           if(message && message != "") {
+               this.socket.emit("chat_message", message);
+           }
+           this.chatBox = "";
+       }
+   } */
 
 }
